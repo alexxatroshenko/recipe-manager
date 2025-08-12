8d040b8a6a39f90f80567361130a018bba484ff0
atroshenkoai <atroshenkoai@ad.atlant.by>
2025-08-12 16:59:15 +0300

    Реализована навигация

--- recipeManager.Web/ClientApp/src/app/components/navigation/navigation.component.ts
+++ recipeManager.Web/ClientApp/src/app/components/navigation/navigation.component.ts
@@ -1,23 +1,38 @@
-import { Component } from '@angular/core';
-import {ThemeService} from '../../common/services/theme.service';
-import {CommonModule} from '@angular/common';
+import { Component, ViewChild } from '@angular/core';
+import { ThemeService } from '../../common/services/theme.service';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { UserInfoComponent } from './user-info/user-info.component';
 
 @Component({
+  standalone: true,
   selector: 'app-nav',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, SidebarComponent, UserInfoComponent],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 })
 export class NavigationComponent {
+  isSideBarOpened: boolean = false;
+  @ViewChild(SidebarComponent) sidebar!: SidebarComponent;
+  @ViewChild(UserInfoComponent) userInfo!: UserInfoComponent;
 
-  constructor(private themeService: ThemeService) {
-  }
+
+  constructor(private themeService: ThemeService) { }
 
   get isDark(): boolean {
     return this.themeService.isDarkMode();
   }
 
-  toggleTheme(){
+  toggleTheme() {
     this.themeService.toggleTheme();
+  }
+
+  toggleSideBar() {
+    this.sidebar.toggleSidebar()
+  }
+
+  toggleUserInfo() {
+    this.userInfo.toggleUserInfo()
   }
 }
