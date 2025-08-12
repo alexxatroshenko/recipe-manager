a042dec469da8c9997c344fdf59cdf059cde2552
atroshenkoai <atroshenkoai@ad.atlant.by>
2025-08-12 16:55:53 +0300

    Обновлен routes

--- recipeManager.Web/ClientApp/src/app/app.routes.ts
+++ recipeManager.Web/ClientApp/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
-import {TestComponent} from './components/test/test.component';
+import { TestComponent } from './components/test/test.component';
+import { RecipesComponent } from './components/recipes/recipes.component';
 
 export const routes: Routes = [
+  { path: "", component: RecipesComponent, pathMatch: "full" },
   { path: "test", component: TestComponent, pathMatch: "full" },
 ];
