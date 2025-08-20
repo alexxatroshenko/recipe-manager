5e6607e2f11df841d2d96c8ca21f135b706ac33c
atroshenkoai <atroshenkoai@ad.atlant.by>
2025-08-20 14:39:36 +0300

    Добавлен путь для создания рецепта

--- recipeManager.Web/ClientApp/src/app/app.routes.ts
+++ recipeManager.Web/ClientApp/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { TestComponent } from './components/test/test.component';
 import { RecipesComponent } from './components/recipes/recipes.component';
+import { CreateRecipeComponent } from './components/recipes/create-recipe/create-recipe.component';
 
 export const routes: Routes = [
   { path: "", component: RecipesComponent, pathMatch: "full" },
   { path: "test", component: TestComponent, pathMatch: "full" },
+  { path: "create-recipe", component: CreateRecipeComponent, pathMatch: "full" },
 ];
