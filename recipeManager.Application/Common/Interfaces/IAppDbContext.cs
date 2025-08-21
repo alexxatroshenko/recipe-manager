2b30895d0e67ff0163753a2003028127b9a89e2c
atroshenkoai <atroshenkoai@ad.atlant.by>
2025-08-21 11:32:21 +0300

    добавлены новые таблицы в application interface

--- recipeManager.Application/Common/Interfaces/IAppDbContext.cs
+++ recipeManager.Application/Common/Interfaces/IAppDbContext.cs
@@ -6,5 +6,11 @@ namespace recipeManager.Application.Common.Interfaces;
 public interface IAppDbContext
 {
     DbSet<Test> Tests { get; }
+    DbSet<User> Users { get; }
+    DbSet<Recipe> Recipes { get; }
+    DbSet<RecipeLike> RecipeLikes { get; }
+    DbSet<RecipeComment> RecipeComments { get; }
+    DbSet<SavedRecipe> SavedRecipes { get; }
+    DbSet<RecipeIngredient> RecipeIngredients { get; }
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);
 }
